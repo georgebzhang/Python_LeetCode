0c28216ae3f00aede64be6735f3355cfe4d546bb
georgebzhang <georgebzhang5@gmail.com>
2019-04-05 21:35:17 -0700

    course schedule

---
+++ 207_course_schedule.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        def dfs(v):
+            if v in visited:
+                return
+            if v in visiting:
+                result[0] = False
+                print('Cycle!')
+                return
+                # raise Exception('Cycle!')
+            visiting.add(v)
+            for n in g[v]:  # for neighbor of vertex
+                dfs(n)
+
+            visiting.remove(v)
+            visited.add(v)
+            order.append(v)
+
+        result = [True]
+        order = []  # topological order
+        visiting = set()
+        visited = set()
+
+        g = defaultdict(list)  # graph
+        for p in prerequisites:
+            g[p[0]].append(p[1])
+            g[p[1]]  # ensures all vertices in graph
+
+        for v in g:  # for vertex in graph
+            dfs(v)
+
+        return result[0]
+
+    def print_ans(self, ans):
+        print(ans)
+
+    def test(self):
+        numCourses = 2
+        prerequisites = [[1, 0]]
+        ans = self.canFinish(numCourses, prerequisites)
+        self.print_ans(ans)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.test()
