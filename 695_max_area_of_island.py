f8f1e6bd8b4eb52aa0f1cbaf99131ef993b0fa0d
georgebzhang <georgebzhang5@gmail.com>
2019-04-07 13:10:00 -0700

    max area of island

---
+++ 695_max_area_of_island.py
@@ -0,0 +1,55 @@
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        def neighbors(p):
+            i, j = p
+            neighbors = []
+            if i-1 >= 0:
+                neighbors.append((i-1, j))
+            if i+1 < len(grid[0]):
+                neighbors.append((i+1, j))
+            if j-1 >= 0:
+                neighbors.append((i, j-1))
+            if j+1 < len(grid):
+                neighbors.append((i, j+1))
+            return neighbors
+
+        def dfs(p):
+            i, j = p
+            if grid[j][i] != 1:
+                return
+            if p in visited:
+                return
+            visited.add(p)
+            island.append(p)
+            for n in neighbors(p):
+                dfs(n)
+
+        visited = set()
+        islands = []
+        for j in range(len(grid)):
+            for i in range(len(grid[0])):
+                p = (i, j)
+                if grid[j][i] == 1 and p not in visited:
+                    island = []
+                    dfs(p)
+                    islands.append(island)
+
+        return 0 if not islands else max([len(island) for island in islands])
+
+    def print_grid(self, grid):
+        for row in grid:
+            print(row)
+
+    def print_ans(self, ans):
+        print(ans)
+
+    def test(self):
+        grid = [[1, 1, 0, 0, 0], [1, 1, 0, 0, 0], [0, 0, 0, 1, 1], [0, 0, 0, 1, 1]]
+        self.print_grid(grid)
+        ans = self.maxAreaOfIsland(grid)
+        self.print_ans(ans)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.test()
